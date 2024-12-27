f90bdfdf78b99a69f10170d2a89b6e5ae2db4776
djordjeivanovic11 <djordjeivanovic65@gmail.com>
2024-12-27 15:24:11 +0100

    done

--- clinify_front/src/app/api/dismiss-popup/route.ts
+++ clinify_front/src/app/api/dismiss-popup/route.ts
@@ -1,13 +1,22 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req) {
+export async function POST() {
   try {
     const headers = new Headers();
-    headers.append("Set-Cookie", "popupDismissed=true; Path=/; Max-Age=2592000; HttpOnly; Secure");
+    headers.append(
+      "Set-Cookie",
+      "popupDismissed=true; Path=/; Max-Age=2592000; HttpOnly; Secure"
+    );
 
-    return NextResponse.json({ message: "Popup dismissed successfully." }, { headers });
+    return NextResponse.json(
+      { message: "Popup dismissed successfully." },
+      { headers }
+    );
   } catch (error) {
     console.error("Failed to dismiss popup:", error);
-    return NextResponse.json({ message: "Failed to dismiss popup." }, { status: 500 });
+    return NextResponse.json(
+      { message: "Failed to dismiss popup." },
+      { status: 500 }
+    );
   }
 }
