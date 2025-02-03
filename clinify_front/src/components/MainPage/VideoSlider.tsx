3dab8882c09dd50a3239f03fff0efcd19d138ce8
Ivan <ivancabrilo.34@gmail.com>
2025-02-03 22:07:17 +0100

    loader fix

--- clinify_front/src/components/MainPage/VideoSlider.tsx
+++ clinify_front/src/components/MainPage/VideoSlider.tsx
@@ -4,7 +4,7 @@ import "./loader.css"; // Ensure this file exists and contains styles
 const VideoSlider: React.FC = () => {
   const videoSources = [
     // "https://www.youtube.com/embed/6CPQbBnwcZ8?modestbranding=1&rel=0&autohide=1&mute=1&autoplay=1&playsinline=1&controls=0",
-    "https://www.youtube.com/embed/OO-xO4cvqg0?si=AN2WmUuy6AENpVu8&modestbranding=1&rel=0&autohide=1&mute=1&autoplay=1&playsinline=1&controls=0",
+    "https://www.youtube.com/embed/OO-xO4cvqg0?si=AN2WmUuy6AENpVu8&modestbranding=1&rel=0&autohide=1&autoplay=1&mute=1&playsinline=1&controls=0",
   ];
 
   const [isLoading, setIsLoading] = useState(true);
@@ -31,8 +31,8 @@ const VideoSlider: React.FC = () => {
       {/* Loader: Shows until video is loaded */}
       {isLoading && (
         <div className="loader absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          {/* <div className="loader-outter"></div>
-          <div className="loader-inner"></div> */}
+          <div className="loader-outter"></div>
+          <div className="loader-inner"></div>
         </div>
       )}
 
