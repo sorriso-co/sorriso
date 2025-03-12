5beab3eddaa2ff9515963f149b2925905ede1a28
Ivan <ivancabrilo.34@gmail.com>
2025-03-12 10:11:47 +0100

    try

--- clinify_front/src/components/Contact/ContactAppointment.tsx
+++ clinify_front/src/components/Contact/ContactAppointment.tsx
@@ -1,24 +1,6 @@
 "use client";
 import { useTranslation } from "react-i18next";
 
-// export default function BookACall() {
-//   const { t } = useTranslation("common");
-
-//   return (
-//     <div className="w-full h-screen flex flex-col items-center justify-center overflow-hidden bg-white rounded">
-//       {/* Header */}
-//       <div className="mb-4" />
-
-//       {/* Fullscreen Iframe */}
-//       <iframe
-//         src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1Z-M-9vG8Fy6-4ZN5H_Ck3v_NOXdIMFFZt9eXxffaFzhL3PeswZYOyjiCd4kdqp276PXFXooxj?gv=true"
-//         className="w-full h-full border-none"
-//         title={t("bookACall.iframe.title")}
-//         aria-label={t("bookACall.iframe.ariaLabel")}
-//       ></iframe>
-//     </div>
-//   );
-// }
 export default function BookACall() {
   const { t } = useTranslation("common");
 
